8df1c2e886abaa7353257cbbce45543900251cf6
Andrew Combs <andrewcombs13@gmail.com>
2016-10-01 13:47:26 -0400

    Switch to inverted output logic
    button off is 1 not 0, update logic accordingly.
    Also be sure to initialize used IO to known state during startup

--- VCR_Controller/VCR_Controller.ino
+++ VCR_Controller/VCR_Controller.ino
@@ -3,20 +3,22 @@ byte buf;
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
-DDRC = B00111111; // set pins 8-13 as output
+   DDRC = B00111111; // set pins 8-13 as output
+   PORTC = PORTC | B00001111;
 }
 void loop() {
-  while(Serial.available()) {
+   while(Serial.available()) {
       data = Serial.read();
       Serial.print(data,BIN); //print out command
       Serial.print("\r\n");
-      data = data &  B00001111 ;
+      data = data ^ B11111111;
+      data = data & B00001111;
       buf = PORTC;
       buf = buf & B11110000;
       buf = buf | data;
       PORTC = buf;
       delay(100);
-      buf = PORTC & B11110000;
+      buf = PORTC | B00001111;
       PORTC = buf;
-  }
+   }
 }
